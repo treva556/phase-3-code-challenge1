49bdb61d68d584a9b437e36cf4459644f11f578d
treva556 <marktreva92@gmail.com>
2023-02-20 15:02:25 +0300

    added changes

--- lib/run.rb
+++ lib/run.rb
@@ -1,11 +1,22 @@
-require_relative './magazine.rb'
-require_relative '../lib/author.rb'
-require_relative '../lib/article.rb'
+require_relative '../magazine.rb'
+require_relative '../author.rb'
+require_relative '../article.rb'
+jeff = Author.new("Jeff")
 
 
 trevs = Article.new("trevs", "good", "lol")
+mag1 = Magazine.new("Vogue", "fashion")
+mag2 = Magazine.new("Vice", "crime")
+mag3 = Magazine.new("Mock", "health")
+mag4 = Magazine.new("Grammy", "music")
+mag5 = Magazine.new("Emmy", "movie")
+
+pp jeff.add_article(mag5, "Not so good people")
+
+
+
 #puts self.all
 puts trevs.author
 #puts self.all_magazines
-puts mag3.name
+#puts mag3.name
 puts mag3.category
